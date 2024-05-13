cc51ebce5f23ab76ace9b8585105cadcac65bbdf
AgnesLigh <131894525+AgnesLigh@users.noreply.github.com>
2024-05-13 20:11:36 +0200

    Update Plot_2.py

--- app/pages/Plot_2.py
+++ app/pages/Plot_2.py
@@ -17,11 +17,26 @@ dash.register_page(__name__,
 
 
 # 示例数据
-data = {
-    'Date': pd.date_range(start='2021-01-01', periods=100, freq='D'),
-    'Sales': np.random.randint(100, 500, size=100)  # 随机生成销售数据
-}
-df = pd.DataFrame(data)
+df = pd.read_csv("https://raw.githubusercontent.com/AgnesLigh/Data_Visualization_D.E.A.D/main/data/orders_nation_season.csv")
+subtype_order = pd.read_csv("https://raw.githubusercontent.com/AgnesLigh/Data_Visualization_D.E.A.D/main/data/subtype_order_month.csv")
+
+df["OrderDate"] = pd.to_datetime(df["OrderDate"],format="%d/%m/%Y")
+df["DeliveryDate"] = pd.to_datetime(df["OrderDate"],format="%d/%m/%Y")
+
+order_counts = df["OrderDate"].value_counts().reset_index()
+order_counts.columns = ["OrderDate", "Number of Orders"]
+order_counts = order_counts.sort_values("OrderDate")
+
+delivery_counts = df["DeliveryDate"].value_counts().reset_index()
+delivery_counts.columns = ["DeliveryDate", "Number of Deliveries"]
+delivery_counts = delivery_counts.sort_values("DeliveryDate")
+
+average_delivery_time = df.groupby('OrderDate')['DeliveryTime'].mean().reset_index()
+
+subtype_fig = px.bar(subtype_order, x='OrderMonth', y='Counts', color='Subtype', 
+                     color_continuous_scale= "Viridis", title='Order Counts by Subtype and Date')
+
+subtype_fig.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'})
 
 
 layout= dbc.Container([
@@ -32,27 +47,58 @@ layout= dbc.Container([
         ], style={"width":"20cm"})
     ]),
     html.Br(),
+  dbc.Row([
     html.Div([
+    dcc.Dropdown(
+        id='date-type-picker',
+        options=["Order Date", "Delivery Date", "Average Delivery Time"],
+        value="Order Date",
+        clearable=False,  
+        style={'width': '50%'}
+    ),
     dcc.DatePickerRange(
         id='date-picker-range',
-        start_date=df['Date'].min(),
-        end_date=df['Date'].max(),
-        display_format='YYYY-MM-DD'
-    ),
-    dcc.Graph(id='sales-bar-chart')
-])
+        start_date=order_counts['OrderDate'].min(),
+        end_date=delivery_counts['DeliveryDate'].max(),
+        display_format='YYYY-MM-DD')
+    ])]),
+    html.Div([
+        dcc.Graph(id='sales-bar-chart')
+    ]),
+    html.Div([
+        dcc.Graph(id='subtype-bar-chart',figure=subtype_fig)
+    ])
 ])
 
+# Callback to update the graph based on the selected date type and date range
 @callback(
     Output('sales-bar-chart', 'figure'),
-    [Input('date-picker-range', 'start_date'),
+    [Input('date-type-picker', 'value'),
+     Input('date-picker-range', 'start_date'),
      Input('date-picker-range', 'end_date')]
 )
-def update_graph(start_date, end_date):
-    # 根据日期过滤数据
-    mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)
-    filtered_df = df.loc[mask]
-    
-    # 使用Plotly创建柱状图
-    fig = px.bar(filtered_df, x='Date', y='Sales', title='Sales by Date')
+def update_graph(selected_date_type, start_date, end_date):
+
+    if selected_date_type == 'Order Date':
+        mask = (order_counts['OrderDate'] >= start_date) & (order_counts['OrderDate'] <= end_date)
+        filtered_order_counts = order_counts.loc[mask]
+        
+        fig = px.bar(filtered_order_counts, x='OrderDate', y='Number of Orders', title='Orders by Date')
+        
+
+    elif selected_date_type == 'Delivery Date':
+        mask = (delivery_counts['DeliveryDate'] >= start_date) & (delivery_counts['DeliveryDate'] <= end_date)
+        filtered_delivery_counts = delivery_counts.loc[mask]
+        
+        fig = px.bar(filtered_delivery_counts, x='DeliveryDate', y='Number of Deliveries', title='Deliveries by Date')
+
+    elif selected_date_type == 'Average Delivery Time':
+        mask = (average_delivery_time['OrderDate'] >= start_date) & (average_delivery_time['OrderDate'] <= end_date)
+        filtered_average_delivery_time = average_delivery_time.loc[mask]
+        
+        fig = px.bar(filtered_average_delivery_time, x='OrderDate', y='DeliveryTime', title='Average Delivery Time by Date',
+                     labels={'DeliveryTime': 'Average Delivery Time'})
+
     return fig
+
+
