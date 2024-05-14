0f62de6e23b5c8b4c04ec4a69a96ee1d1fe153f7
yutiiingma <135553863+yutiiingma@users.noreply.github.com>
2024-05-14 12:40:26 +0200

    conclusion.py

--- app/pages/conclusion.py
+++ app/pages/conclusion.py
@@ -18,8 +18,11 @@ dash.register_page(__name__,
 layout= dbc.Container([
   dbc.Row([
         html.Div([
-            html.H3(children="To explore the relationship between 'where' and 'what' "),
-            html.P("This plot aims to find possible relationship between ...")
+            html.H3(children="Conclusions and Key findings"),
+            html.P(children="Our three guiding questions:"),
+            html.P(children="1. How can we visualize the delivery times across the regions of Faerun to find areas of improvement in shipping times for the DEAD company?"),
+            html.P(children="2. What is the best way to map out the best and worst selling products sold by the DEAD company?"),
+            html.P(children="3. How can we gauge the popularity of the DEAD company across the regions of the Forgotten Realms?")
         ], style={"width":"20cm"})
     ]),
     html.Br()
