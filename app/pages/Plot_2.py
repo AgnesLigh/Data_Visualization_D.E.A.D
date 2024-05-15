039f9630c9d8c556f8807fad36d69c616709b6d2
AgnesLigh <131894525+AgnesLigh@users.noreply.github.com>
2024-05-15 14:29:27 +0200

    Update Plot_2.py

--- app/pages/Plot_2.py
+++ app/pages/Plot_2.py
@@ -43,52 +43,51 @@ layout= dbc.Container([
             html.H3(children="To explore the relationship between 'when' and 'what' "),
             html.P(children="In this page, you will find the visualization of the order and delivery related information. There are mainly two parts of the visualization."),
             html.P(children="In the first part, we focus on 'when'. As you can find the amount of order and delivery at different time frame of your interest. Additionally, you can also see the average delivery time based on the two information above. For the time frame of your interest, you can first click and select in the calender for a general time frame. Then you can drag-select a more specific time frame to have a closer look at the fluctuation of order/delivery/average delivery time. (Small tip: if you double click on the bar chart of specific time frame, you can go back to the general time frame selected from calender) ")
-        ], style={"width":"20cm"})
+        ], style={"width":"25cm"})
     ]),
     html.Br(),
   dbc.Row([
     html.Div([
-    dcc.Dropdown(
+        dcc.Dropdown(
         id='date-type-picker',
         options=["Order Date", "Delivery Date", "Average Delivery Time"],
         value="Order Date",
         clearable=False,  
-        style={'width': '50%'}
-    ),
-    dcc.DatePickerRange(
+        style={'width': '50%'}),
+        dcc.DatePickerRange(
         id='date-picker-range',
         min_date_allowed=order_counts['OrderDate'].min(),
         max_date_allowed=delivery_counts['DeliveryDate'].max(),
         start_date='2019-01-01',
         end_date='2023-12-31',
         display_format='YYYY-MM-DD')
-    ])]),
+        ], style={'display': 'flex', 'justify-content': 'left'})
+    ]),
     html.Div([
         dcc.Graph(id='sales-bar-chart')
     ]),
     dbc.Row([
         html.Div([
             html.P("For the second part, we combine 'when' and 'what'. You can see the whole distribution of all the subtypes of product within certain time frame of your choice. You can select a sepecfic subtype or few subtypes of interest to see the distribution. The drag-select function for a more specific time frame and the doubble-click resetting function can also be applied here. ")
-        ], style={"width":"20cm"})
+        ], style={"width":"25cm"})
     ]),
     html.Br(),
     dbc.Row([
     html.Div([
-    dcc.Dropdown(
+        dcc.Dropdown(
         id='subtype-date-type-picker',
         options=["Order Date", "Delivery Date"],
         value="Order Date",
         clearable=False,  
-        style={'width': '50%'}
-    ),
-    dcc.DatePickerRange(
+        style={'width': '50%'}),
+        dcc.DatePickerRange(
         id='subtype-date-picker-range',
         min_date_allowed=order_counts['OrderDate'].min(),
         max_date_allowed=delivery_counts['DeliveryDate'].max(),
         start_date='2019-01-01',
         end_date='2019-12-31',
         display_format='YYYY-MM-DD')
-    ])
+    ], style={'display': 'flex', 'justify-content': 'left'})
     ]),
     html.Div([
         dcc.Graph(id='subtype-bar-chart')
@@ -109,13 +108,14 @@ def update_graph(selected_date_type, start_date, end_date):
         filtered_order_counts = order_counts.loc[mask]
         
         fig = px.bar(filtered_order_counts, x='OrderDate', y='Number of Orders', title='Orders by Date')
-        
+        fig.update_layout(width=1000, height=500)
 
     elif selected_date_type == 'Delivery Date':
         mask = (delivery_counts['DeliveryDate'] >= start_date) & (delivery_counts['DeliveryDate'] <= end_date)
         filtered_delivery_counts = delivery_counts.loc[mask]
         
         fig = px.bar(filtered_delivery_counts, x='DeliveryDate', y='Number of Deliveries', title='Deliveries by Date')
+        fig.update_layout(width=1000, height=500)
 
     elif selected_date_type == 'Average Delivery Time':
         mask = (average_delivery_time['OrderDate'] >= start_date) & (average_delivery_time['OrderDate'] <= end_date)
@@ -123,6 +123,7 @@ def update_graph(selected_date_type, start_date, end_date):
         
         fig = px.bar(filtered_average_delivery_time, x='OrderDate', y='DeliveryTime', title='Average Delivery Time by Date',
                      labels={'DeliveryTime': 'Average Delivery Time'})
+        fig.update_layout(width=1000, height=500)
 
     return fig
 
@@ -140,7 +141,8 @@ def update_graph(selected_date_type, start_date, end_date):
         fig = px.bar(filtered_subtype_order, x='OrderDate', y='Counts', color='Subtype', 
                  color_continuous_scale= "Viridis", title='Order Counts by Subtype and Date')
 
-        fig.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'})
+        fig.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'},
+                          width=1000, height=500)
 
     elif selected_date_type == 'Delivery Date':
         mask = (subtype_delivery['DeliveryDate'] >= start_date) & (subtype_delivery['DeliveryDate'] <= end_date)
@@ -148,6 +150,7 @@ def update_graph(selected_date_type, start_date, end_date):
         fig = px.bar(filtered_subtype_delivery, x='DeliveryDate', y='Counts', color='Subtype', 
                  color_continuous_scale= "Viridis", title='Order Counts by Subtype and Date')
 
-        fig.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'})
+        fig.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'},
+                          width=1000, height=500)
 
     return fig
